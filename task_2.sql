8e80fcaa7718d79d7bf1278988aca97394575d47
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-07-27 23:26:55 +0300

    task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -3,6 +3,7 @@ USE alx_book_store;
 ["Authors", "author_id ", "author_name"]
 ["Books", "title", "price"]
 ["Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
+["Orders", "order_id INT", "customer_id INT", "order_date DATE", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)"]
 -- Create the authors table
 CREATE TABLE IF NOT EXISTS AUTHORS (
     AUTHOR_ID INT AUTO_INCREMENT PRIMARY KEY,
